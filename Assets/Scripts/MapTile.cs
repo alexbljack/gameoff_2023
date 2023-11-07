504144f0b7bbe18765e0fbd0df92a32236718495
Aleksandr Shinkarev <alexbljack@gmail.com>
2023-11-07 23:56:07 +0500

    Add basic tiles sprites

--- Assets/Scripts/MapTile.cs
+++ Assets/Scripts/MapTile.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class MapTile : MonoBehaviour
 {
+    // Start is called before the first frame update
     void Start()
     {
-        var _rndr = GetComponent<SpriteRenderer>();
+        
     }
 
     // Update is called once per frame
