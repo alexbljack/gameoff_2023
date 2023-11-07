b90e030e9dc8b683e690d8da352a99856116a01b
Aleksandr Shinkarev <alexbljack@gmail.com>
2023-11-07 23:36:22 +0500

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Start()
+    {
+        var _rndr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
